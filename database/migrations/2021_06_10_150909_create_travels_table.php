b916520f0e2634fe582fbedaccc62502246b0bb1
MarcoBagnasco <bagna.hbk@gmail.com>
2021-06-10 18:03:52 +0200

    create migration and update

---
+++ database/migrations/2021_06_10_150909_create_travels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('main_location', 50);
+            $table->double('price', 6, 2)->unsigned();
+            $table->string('duration', 50);
+            $table->dateTime('departure');
+            $table->boolean('available')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}

1339171152b965c86e950e3e8b344e1c84f8ad2f
MarcoBagnasco <bagna.hbk@gmail.com>
2021-06-10 22:54:59 +0200

    print travels

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Travel;
+
 
 class TravelController extends Controller
 {
@@ -10,6 +12,8 @@ class TravelController extends Controller
      * TRAVELS
      */
     public function index(){
-        return view('travels');
+        $travels = Travel::simplePaginate(10);
+
+        return view('travels', compact('travels'));
     }
 }
--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -3,6 +3,21 @@
 @section('content')
     <section class="travels">
         <h1>OFFERS</h1>
+        @foreach ($travels as $travel)
+        <div class="card">
+            <h3>{{ $travel->main_location }}</h3>
+            <h4>€ {{ number_format($travel->price, 2, ',', '.') }}</h4>
+            <ul>
+                <li>Duration: {{ $travel->duration }}</li>
+                <li>Departure: {{ $travel->departure }}</li>
+                <li>Itinerary: {{ $travel->itinerary }}</li>
+            </ul>
+        </div>
+        @endforeach
+
+        <div>
+            {{ $travels->links() }}
+        </div>
         <a href="{{ route('home') }}">Back to Home</a>
     </section>
 @endsection
